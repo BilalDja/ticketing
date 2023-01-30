b6de06000465e93d080be00cf13d5cc881c64e05
Bilal Djaghout <b.djaghout@gmail.com>
2023-01-31 00:03:28 +0100

    Remove a dot

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,7 +7,7 @@ import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listene
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
-  console.log("Start Orders Service...");
+  console.log("Start Orders Service..");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY env is not set");
   }
