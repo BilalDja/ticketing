680afaecdb571d45e31afda1b1e28458c29b37b9
Bilal Djaghout <b.djaghout@gmail.com>
2023-01-30 22:44:50 +0100

    add starting message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  console.log("String Auth service...");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY env is not set");
   }
